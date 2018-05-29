2b45579e8d98057f0c3859d5066642c29073d99c
Amila Welihinda <amilajack@gmail.com>
2018-05-28 19:12:18 -0700

    Orbit controls

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three/build/three.module.js';
+import WindowResize from 'threejs-window-resize';
+import './OrbitControls';
 
 // Scene configuration
 const scene = new THREE.Scene();
@@ -9,20 +11,58 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// material texture
+const texture = new THREE.Texture(generateTexture());
+texture.needsUpdate = true; // important!
+const gradientMaterial = new THREE.MeshBasicMaterial( { map: texture, transparent: true } );
+
+function generateTexture() {
+	var size = 512;
+
+	// create canvas
+	const canvas = document.createElement( 'canvas' );
+	canvas.width = size;
+	canvas.height = size;
+
+	// get context
+	var context = canvas.getContext( '2d' );
+
+	// draw gradient
+	context.rect( 0, 0, size, size );
+	var gradient = context.createLinearGradient( 0, 0, size, size );
+	gradient.addColorStop(0, '#99ddff'); // light blue 
+	gradient.addColorStop(1, 'transparent'); // dark blue
+	context.fillStyle = gradient;
+	context.fill();
+
+	return canvas;
+}
+
 // Drawing an object to the scene
 const geometry = new THREE.BoxGeometry( 3, 2, 4 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+const material = new THREE.MeshBasicMaterial({
+	color: 0x00ff00,
+	wireframe: true
+});
+const cube = new THREE.Mesh( geometry, gradientMaterial );
 scene.add(cube);
+scene.add( new THREE.AxesHelper( 20 ) );
 
 // Camera position
 camera.position.z = 5;
 
+// Orbit controls
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', () => {
+	renderer.render(scene, camera);
+});
+
+WindowResize(renderer, camera);
+
 // Render loop
 function animate() {
-    cube.rotation.x += 0.1;
-    // cube.rotation.y += 0.1;
-    // cube.rotation.z += 0.1;
+	controls.update()
+    cube.rotation.x += 0.05;
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
