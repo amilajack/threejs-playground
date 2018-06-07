35e1a4b7646f90d9c08bb5a1a0b107781fbbed61
Amila Welihinda <amilajack@gmail.com>
2018-06-06 17:50:32 -0700

    Removed lights

--- index.js
+++ index.js
@@ -16,40 +16,17 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.gammaInput = true;
 renderer.gammaOutput = true;
 
-// material texture
-const texture = new THREE.Texture(generateTexture());
-texture.needsUpdate = true; // important!
-const gradientMaterial = new THREE.MeshBasicMaterial( { map: texture, transparent: true } );
-
-function generateTexture() {
-	const size = 512;
-
-	// create canvas
-	const canvas = document.createElement( 'canvas' );
-	canvas.width = size;
-	canvas.height = size;
-
-	// get context
-	var context = canvas.getContext( '2d' );
-
-	// draw gradient
-	context.rect( 0, 0, size, size );
-	var gradient = context.createLinearGradient( 0, 0, size, size );
-	gradient.addColorStop(0, '#99ddff'); // light blue 
-	gradient.addColorStop(1, 'transparent'); // dark blue
-	context.fillStyle = gradient;
-	context.fill();
-
-	return canvas;
-}
-
 // Drawing an object to the scene
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial({
 	color: 0x00ff00,
 	wireframe: true
 });
-const cube = new THREE.Mesh( geometry, gradientMaterial );
+const boxMaterial = new THREE.MeshBasicMaterial({
+	color: 0xAAAAAA
+})
+const cube = new THREE.Mesh( geometry,  boxMaterial);
+cube.position.set(0, 0, -10)
 scene.add(cube);
 scene.add( new THREE.AxesHelper( 20 ) );
 
@@ -62,33 +39,6 @@ controls.addEventListener('change', () => {
 	renderer.render(scene, camera);
 });
 
-// Add lights
-// const light = new THREE.AmbientLight(0x404040);
-// light.position.set(25, 20, 25);
-// light.castShadow = true;
-// scene.add(light);
-var spotLight = new THREE.SpotLight( 0xffffff );
-spotLight.position.set( 100, 1000, 100 );
-
-spotLight.castShadow = true;
-
-spotLight.shadow.mapSize.width = 1024;
-spotLight.shadow.mapSize.height = 1024;
-
-spotLight.shadow.camera.near = 500;
-spotLight.shadow.camera.far = 4000;
-spotLight.shadow.camera.fov = 30;
-
-let lightHelper = new THREE.SpotLightHelper( spotLight );
-
-var ambient = new THREE.AmbientLight( 0xffffff, 0.1 );
-scene.add( ambient );
-
-const shadowCameraHelper = new THREE.CameraHelper( spotLight.shadow.camera );
-scene.add( shadowCameraHelper );
-
-scene.add( spotLight );
-
 WindowResize(renderer, camera);
 
 // Render loop
@@ -97,7 +47,5 @@ function animate() {
     cube.rotation.x += 0.05;
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
-	lightHelper.update();
-	shadowCameraHelper.update();
 }
 animate();
